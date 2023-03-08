62de467b4fae656ce8cf3811210cbe96c6742b3b
BUDI SETIAWAN <budi.setiawan@kyndryl.com>
2023-03-08 16:49:19 +0800

    fix code error

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ if (port == null || port == "") {
 }
 app.listen(port, function () {
   console.log("Server has started successfully");
-}));
+});
 
 //app.listen(process.env.PORT||3000, function() {
 //  console.log("Server started on port 3000");
